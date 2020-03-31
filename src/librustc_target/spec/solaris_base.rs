e6cd4e5e2c132244e6f885f1994c39923f631b3d
Joshua M. Clulow <jmc@oxide.computer>
2020-03-30 21:36:28 -0700

    XXX no blanket -nodefaultlibs usage

--- src/librustc_target/spec/solaris_base.rs
+++ src/librustc_target/spec/solaris_base.rs
@@ -6,6 +6,9 @@ pub fn opts() -> TargetOptions {
         dynamic_linking: true,
         executables: true,
         has_rpath: true,
+        // Under some conditions when building C code, GCC may need to link
+        // additional libraries; e.g., libssp.so.  Allow it to do so:
+        no_default_libraries: false,
         target_family: Some("unix".to_string()),
         is_like_solaris: true,
         limit_rdylib_exports: false, // Linker doesn't support this
